3f9f19e873832769d65e54e21b76d3ac6373336a
MhmdSafa7 <mhmdhsafa7@gmail.com>
2025-01-07 22:34:14 +0200

    step1

---
+++ app/Models/Menu.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    public $timestamps=false;
+
+    // Table name
+    public $table='menus';
+
+    // Primary Key
+    public $primaryKey='id';
+
+    use HasFactory;
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+}
